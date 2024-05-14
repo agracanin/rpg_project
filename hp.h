b270bd7c4dc1a937a30ae8467453f9cf4b57fbe9
aleng <alengracanin@gmail.com>
2024-05-14 15:29:06 -0400

    Add project files.

---
+++ hp.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "hitpointtypes.h"
+
+
+class hp {
+public:
+	// return true if set successfully
+	bool setMaxHP(hptype new_max_hp) {
+		if (new_max_hp < 1) {
+			return false;
+		}
+
+		MaxHP = new_max_hp;
+
+		if (CurrentHP > MaxHP) {
+			CurrentHP = MaxHP;
+		}
+
+		return true;
+	}
+
+	hptype getMaxHP() {
+		return MaxHP;
+	}
+
+	hptype getCurrentHP() {
+		return CurrentHP;
+	}
+
+	void takeDamage(hptype damage) {
+		if (damage > CurrentHP + ShieldHP) {
+			CurrentHP = 0;
+			return;
+		}
+		// Todo: Shield Check
+		CurrentHP -= damage;
+	}
+
+	void heal(hptype amount) {
+		if (amount + CurrentHP > MaxHP) {
+			CurrentHP = MaxHP;
+			return;
+		}
+
+		CurrentHP += amount;
+	}
+
+
+private:
+	hptype ShieldHP;
+	hptype CurrentHP;
+	hptype MaxHP;
+};
